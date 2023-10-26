833d0b66b6785922fc1c793c32a9267bd34c3a9d
Pablo Carrasco <pablo.carrasco@uc.cl>
2023-10-25 23:36:30 -0300

    Conexión con postgres

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import express from "express"
+import pg from "pg"
 import { config } from "dotenv";
 config()
 
 const app = express()
+new pg.Pool({
+    connectionString: process.env.PSQL_DATABASE_URL,
+    // ssl: true
+})
 
 app.get('/', (req, res) => {
     res.send('Holaa Mundo de Desarrollo de Software!')
+})
+
+app.get('/ping', async (req, res) => {
+    const result = await Pool.query('SELECT NOW()')
+    return res.json(result.rows[0])
 })
 
 app.listen(process.env.NODE_DOCKER_PORT)

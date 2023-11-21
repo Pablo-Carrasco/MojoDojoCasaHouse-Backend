c6979e2e7a427d89827b4f7667f737963a55acf7
DENISSE LEIVA <denisse.leiva@uc.cl>
2023-11-20 21:56:18 -0300

    fix: fixed test

--- src/index.test.js
+++ src/index.test.js
@@ -13,15 +13,15 @@ beforeAll(async () => {
     name: 'Cine Prueba',
       location: loc,
       createdAt: new Date(),
-      updatedAt: new Date()
+      updatedAt: new Date(),
+      id: 4
   })
-  var {count, rows} = await db.Cinema.findAndCountAll()
   const testShow = await db.Show.create({
     title: "Batman Prueba",
       schedule: "12:00:00",
       link_to_show: "aaalink1",
       link_to_picture: "aaalink2",
-      id_cinema: count,
+      id_cinema: 4,
       createdAt: new Date(),
       updatedAt: new Date(),
       date: new Date("2023-11-27")
@@ -30,9 +30,7 @@ beforeAll(async () => {
 
 afterAll(async () => {
   const testShow = await db.Show.findOne({ where: { title: 'Batman Prueba' } })
-  console.log(await db.Cinema.findAll())
-  const testCinema = await db.Cinema.findOne({ where: { title: 'Cine Prueba' }})
-  console.log(testCinema)
+  const testCinema = await db.Cinema.findOne({ where: { name: 'Cine Prueba' }})
   await testShow.destroy()
   await testCinema.destroy()
   await db.sequelize.close()
@@ -46,7 +44,7 @@ describe("Shows", () => {
         "location": "Latitude: -33.417052, Longitude: -70.5100854",
         "movie": "Batman Prueba 3",
         "currentLocation": true,
-        "date": "2023-11-15"
+        "date": "2023-11-28"
     })
         expect(res.statusCode).toEqual(200)
         expect(res.body).toEqual([ [], 'Batman Prueba 3' ])
@@ -60,11 +58,26 @@ describe("Shows", () => {
         "location": "Latitude: -33.0000000, Longitude: -70.0000000",
         "movie": "Batman Prueba",
         "currentLocation": true,
-        "date": "2023-11-27"
+        "date": "2023-11-28"
     })
         expect(res.statusCode).toEqual(200)
-        console.log(res.body)
-        expect(res.body).toEqual([ [], 'Batman Prueba' ])
+        var json = JSON.parse(JSON.stringify(res.body[0]))
+        expect(res.body[1]).toEqual('Batman Prueba')
+        expect(json[0].name).toEqual('Cine Prueba') 
+  });
+  it("test-NoShowsWithDate", async () => {
+      const res = await request(app)
+      .post('/search')
+      .send({
+        "location": "Latitude: -33.0000000, Longitude: -70.0000000",
+        "movie": "Batman Prueba",
+        "currentLocation": true,
+        "date": "2023-11-15"
+    })
+        expect(res.statusCode).toEqual(200)
+        var json = JSON.parse(JSON.stringify(res.body[0]))
+        expect(res.body[1]).toEqual('Batman Prueba')
+        expect(res.body[0]).toEqual([])
   });
 
 })
